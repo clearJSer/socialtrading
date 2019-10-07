a9c8398b2941adbe483f737da01afb4c5c640810
liuliang01 <liuliang01@sogou-inc.com>
2019-10-07 14:07:11 +0800

    add /docs

---
+++ docs/precache-manifest.ca22fec5ac8e6b951d6b9dddc5e8bba8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/socialtrading/robots.txt"
+  },
+  {
+    "revision": "8f930fe1bee44f065ae0",
+    "url": "/socialtrading/js/chunk-vendors.9e3f1f41.js"
+  },
+  {
+    "revision": "bbf83724c080f56d0d67",
+    "url": "/socialtrading/js/app.6523e534.js"
+  },
+  {
+    "revision": "2186c1c8c29ef8bd8ffe",
+    "url": "/socialtrading/js/about.1e9fdcc0.js"
+  },
+  {
+    "revision": "c74b83bb592f9fbfc0dd1a56619a7cc1",
+    "url": "/socialtrading/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/socialtrading/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bbf83724c080f56d0d67",
+    "url": "/socialtrading/css/app.1b102099.css"
+  }
+];
